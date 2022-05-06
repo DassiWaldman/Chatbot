f08abc41ae7042e416611a395b2e8d245ebb9378
Dassi Waldman <vald10965@outlook.co.il>
2022-05-06 14:53:25 +0300

    Add plugin print date and time

---
+++ DateTimePlugin/DateTimeP.cs
@@ -0,0 +1,22 @@
+﻿using BasePlugin;
+using BasePlugin.Interfaces;
+using BasePlugin.Records;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System;
+
+namespace DateTimePlugin
+{
+    public class DateTimeP : IPlugin
+    {
+        public static string _Id => "date-time";
+        public string Id => _Id;
+
+        public PluginOutput Execute(PluginInput input)
+        {
+            return new PluginOutput(DateTime.Now.ToString());
+        }
+    }
+}
